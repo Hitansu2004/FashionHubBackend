eb6ee5e0ec9109f50cad3fc25c88c1fddffa3f85
Bhagya Sri Balaga <bbalaga@nisum.com>
2025-07-18 17:31:35 +0530

    Security class added

--- inventoryService/src/main/java/com/nisum/inventoryService/service/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.nisum.inventoryService.service;
-
-import com.nisum.inventoryService.dto.LoginDTO;
-import com.nisum.inventoryService.dto.LoginResponseDTO;
-
-public interface LoginService {
-    LoginResponseDTO validateLogin(LoginDTO dto);
-}
